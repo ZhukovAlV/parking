73c67fd310ab173dc664b8ff045c25048975c713
plinio <avzhykov@gmail.com>
2020-05-30 18:08:25 +0600

    create model and service

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Car {
+	
+    private String carNum;
+    private String carType;
+    
+	public String getCarNum() {
+		return carNum;
+	}
+	public void setCarNum(String carNum) {
+		this.carNum = carNum;
+	}
+	public String getCarType() {
+		return carType;
+	}
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+  
+    
+}
---
+++ src/main/java/model/TypeCar.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum TypeCar {
+// Пока 2 типа: пассажирская и грузовая машина 
+	PASSENGER, TRUCK
+}
